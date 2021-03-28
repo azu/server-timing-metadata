87d58644d7053addb97401a894dc3bb07a7fd072
azu <azuciao@gmail.com>
2021-03-28 12:08:36 +0900

    update

--- pages/api/server-timing.js
+++ pages/api/server-timing.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     res.statusCode = 200;
     // cache 5min
-    res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
+    res.setHeader('Cache-Control', 'max-age=5, s-maxage=10');
     res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
     // Server Timing
     res.setHeader('Server-Timing', `unixTime;desc="Unix TimeStamp";dur=${Date.now()}`);
